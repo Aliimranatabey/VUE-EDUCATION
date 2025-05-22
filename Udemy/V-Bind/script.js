48e5c4db2ec7b748a03fcc41aa7ef40f51d6bf38
Ali Imran Atabey <65621472+Aliimranatabey@users.noreply.github.com>
2025-05-22 20:33:15 +0300

    v-bind directive kullanımı

---
+++ Udemy/V-Bind/script.js
@@ -0,0 +1,10 @@
+const app=Vue.createApp({
+    data(){
+        return{
+            firstGoal:'HTML',
+            myLinkedin:'https://www.linkedin.com/in/aliimranatabey/',
+        };
+    },
+});
+
+app.mount('#frontend')
