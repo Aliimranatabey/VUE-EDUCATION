97267507d4b83fa0093cb8486d52cb2788e85a02
Ali Imran Atabey <65621472+Aliimranatabey@users.noreply.github.com>
2025-05-25 22:58:37 +0300

    egzersiz yapıldı

---
+++ Udemy/7-Egzersiz/script.js
@@ -0,0 +1,13 @@
+const app=Vue.createApp({
+    data(){
+        return{
+            name:'Ali İmran Atabey',
+            age:24,
+            imageUrl:'https://plus.unsplash.com/premium_photo-1747375615029-515941ee7a99?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        };
+    },
+    methods: {
+    },
+});
+
+app.mount('#frontend')
