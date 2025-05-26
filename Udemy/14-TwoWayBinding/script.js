f1f131ca887f473cd82aba28eb56d879fcc214cc
Ali Imran Atabey <65621472+Aliimranatabey@users.noreply.github.com>
2025-05-26 21:02:42 +0300

    TwoWayBinding işlemini v-model ile yapmayı öğrendik

---
+++ Udemy/14-TwoWayBinding/script.js
@@ -0,0 +1,30 @@
+const app=Vue.createApp({
+    data(){
+        return{
+            counter:0,
+            name:'Ali İmran',
+        };
+    },
+    methods: {
+        reset(event){
+            this.name=''
+
+        },
+        login(event){
+            // event.preventDefault();
+            alert('Giriş Yapıldı');
+        },
+        setName(event){
+            // debugger;
+            this.name=event.target.value;
+        },
+        add(number){
+            this.counter=this.counter+number;
+        },
+        reduce(number){
+            this.counter=this.counter-number;
+        },
+    },
+});
+
+app.mount('#frontend')
