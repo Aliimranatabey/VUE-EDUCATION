50fdd666ff0747faecd920904872d6548baa128c
Ali Imran Atabey <65621472+Aliimranatabey@users.noreply.github.com>
2025-05-26 20:09:20 +0300

    Input Event kullanımı gerçekleştirildi

---
+++ Udemy/11-InputEvent/script.js
@@ -0,0 +1,22 @@
+const app=Vue.createApp({
+    data(){
+        return{
+            counter:0,
+            name:'Ali İmran',
+        };
+    },
+    methods: {
+        setName(event, lastname){
+            // debugger;
+            this.name=event.target.value + ' ' + lastname;
+        },
+        add(number){
+            this.counter=this.counter+number;
+        },
+        reduce(number){
+            this.counter=this.counter-number;
+        },
+    },
+});
+
+app.mount('#frontend')
