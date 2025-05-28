222af420d516678e10443ab0f48fd2ba0fbf317c
Ali Imran Atabey <65621472+Aliimranatabey@users.noreply.github.com>
2025-05-28 13:37:26 +0300

    Konu tekrarı yapıldı

---
+++ Udemy/20-ChatGPTTekrarEgzersizi/script.js
@@ -0,0 +1,25 @@
+const app=Vue.createApp({
+    data(){
+        return{
+            number1:0,
+            number2:0,
+        };
+    },
+    watch:{
+        
+    },
+    computed:{
+        sum(){
+            console.log('Computed Tetiklendi')
+            return this.number1+this.number2;
+        }
+    },
+    methods: {
+        sumMethod(){
+            console.log('Method Tetiklendi')
+            return this.number1+this.number2;
+        }
+    },
+});
+
+app.mount('#frontend')
