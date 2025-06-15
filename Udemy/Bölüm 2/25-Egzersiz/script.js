89d1d43acdae9cbbab42f70cf69d48dbb606d9ce
Ali Imran Atabey <65621472+Aliimranatabey@users.noreply.github.com>
2025-06-15 20:05:09 +0300

    Dinamik stillendirmeleri içeren bir egzersiz yapıldı

---
+++ Udemy/Bölüm 2/25-Egzersiz/script.js
@@ -0,0 +1,13 @@
+const app=Vue.createApp({
+    data() {
+        return {
+            inputBackGround:'',
+            inputColor:''
+        }
+    },
+    watch:{},
+    computed:{},
+    methods: {},
+});
+
+app.mount('#frontend')
