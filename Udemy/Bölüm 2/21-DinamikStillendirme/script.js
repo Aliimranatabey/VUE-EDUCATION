f57e884519e718b99affb182a2b4539f514e63bb
Ali Imran Atabey <65621472+Aliimranatabey@users.noreply.github.com>
2025-06-15 18:56:30 +0300

    Dinamik Stillendirme ve Dinamik Inline çalışıldı.

---
+++ Udemy/Bölüm 2/21-DinamikStillendirme/script.js
@@ -0,0 +1,22 @@
+const app=Vue.createApp({
+    data() {
+        return {
+            firstBox : false,
+            secondBox : false,
+            thirdBox : false
+        }
+    },
+    watch:{
+
+    },
+    computed:{
+
+    },
+    methods: {
+        chooseBox(number){
+
+        }
+    },
+});
+
+app.mount('#frontend')
