c84e1ce5eb78ed4cb43c462f1214ea83034193d2
Ali Imran Atabey <65621472+Aliimranatabey@users.noreply.github.com>
2025-12-22 23:42:10 +0300

    saldırı oyunu yapılıyor

---
+++ Udemy/Bölüm 4 - VUE.JS PROJE SALDIRI OYUNU (OPTIONS API)/34-KarsilikliSaldiri/script.js
@@ -0,0 +1,31 @@
+function getRandomValue(min,max){
+    return Math.floor(Math.random()*(max-min))+min;
+}
+
+
+const app=Vue.createApp({
+    data(){
+        return{
+            winner:null,
+            myHealth:100,
+            computerHealth:100,
+        };
+    },
+    watch:{    },
+    computed:{    },
+    methods: {   
+        attackToComputer(){
+            const attackValue=getRandomValue(7,15)
+            this.computerHealth=this.computerHealth-attackValue;
+            this.attackMe()
+
+        },
+        attackMe(){
+                const attackValue=getRandomValue(10,20)
+                this.myHealth=this.meHealth-attackValue;
+
+        }
+      },
+});
+
+app.mount('#frontend')
