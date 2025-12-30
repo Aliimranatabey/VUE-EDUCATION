3bfe08f5db1eff577a1f9ab274e9024b7a14be70
Ali Imran Atabey <65621472+Aliimranatabey@users.noreply.github.com>
2025-12-30 12:23:00 +0300

    Feat: Yeni saldırı oyunu modülü eklendi, sağlık ve saldırı mekanikleri güncellendi

---
+++ Udemy/Bölüm 4 - VUE.JS PROJE SALDIRI OYUNU (OPTIONS API)/38-SuperSaldiriKosul/script.js
@@ -0,0 +1,69 @@
+function getRandomValue(min,max){
+    return Math.floor(Math.random()*(max-min))+min;
+}
+
+
+const app=Vue.createApp({
+    data(){
+        return{
+            winner:null,
+            myHealth:100,
+            computerHealth:100,
+            currentRound:0,
+        };
+    },
+    watch:{  },
+    computed:{  
+        computerBarStyles(){
+            if (this.computerHealth<0){
+                return {width:'0%'}
+            }else{
+            return {width:this.computerHealth + '%'}
+            }
+        },
+        myBarStyles(){
+            if (this.myHealth<0){
+                return {width:'0%'}
+            }else{
+            return {width:this.myHealth + '%'}
+            }
+        },
+        possibleSpecialAttack(){
+            return this.currentRound %4 !== 0;
+        }
+      },
+    methods: {   
+        attackToComputer(){
+            this.currentRound++;
+            const attackValue=getRandomValue(7,15)
+            this.computerHealth=this.computerHealth-attackValue;
+            this.attackMe()
+
+        },
+        attackMe(){
+            const attackValue=getRandomValue(10,20)
+            this.myHealth=this.myHealth-attackValue;
+
+        },
+        specialAttack(){
+            this.currentRound++;
+            const attackValue=getRandomValue(15,30)
+            this.computerHealth=this.computerHealth-attackValue;
+            this.attackMe()
+
+        },
+        healMe(){
+            this.currentRound++;
+            const healthValue=getRandomValue(15,20);
+            if(this.myHealth + healthValue >100){
+                this.myHealth=100;
+            }else{  
+                this.myHealth=this.myHealth + healthValue;
+            }
+            this.attackMe();
+
+        }
+      },
+});
+
+app.mount('#frontend')
