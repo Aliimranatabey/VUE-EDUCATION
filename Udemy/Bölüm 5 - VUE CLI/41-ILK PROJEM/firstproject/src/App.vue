f4b67f1f1e89fa6c89fe914b0d2416a62d1e55c6
Ali Imran Atabey <65621472+Aliimranatabey@users.noreply.github.com>
2026-01-03 16:02:10 +0300

    feat: vue cli ile gerçek bir vue projesi oluşturuldu.

---
+++ Udemy/Bölüm 5 - VUE CLI/41-ILK PROJEM/firstproject/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <h2>Öğrencilerim</h2>
+  <ul v-for="student in students" :key="student.name">
+    <li>{{ student.name }} - {{ student.phone }}</li>
+  </ul>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      students: [
+        { name: 'Ahmet', phone: '555-1234' },
+        { name: 'Ayşe', phone: '555-5678' },
+        { name: 'Mehmet', phone: '555-8765' }
+      ]
+    }
+  }
+}
+</script>
+
